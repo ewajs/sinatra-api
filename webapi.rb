fc1b3c7cdebbfa0bd941a62a1dbef8dea87d31d6
Ezequiel Wajs <ezequiel.wajs@gmail.com>
2021-01-25 10:01:15 -0300

    Added specific URLs for JSON and XML. Non URI RFC/HTTP Compliant

--- webapi.rb
+++ webapi.rb
@@ -1,20 +1,37 @@
+# webapi.rb
 require 'sinatra'
 require 'json'
 
+# We require gyoku here in order to use it in the
+# XML route.
+require 'gyoku'
+
 users = {
-  'thibault': { first_name: 'Thibault', last_name: 'Denizet', age: 25 },
-  'simon':    { first_name: 'Simon', last_name: 'Random', age: 26 },
-  'john':     { first_name: 'John', last_name: 'Smith', age: 28 }
+  thibault: { first_name: 'Thibault', last_name: 'Denizet', age: 25 },
+  simon:    { first_name: 'Simon', last_name: 'Random', age: 26 },
+  john:     { first_name: 'John', last_name: 'Smith', age: 28 }
 }
 
 before do
-    content_type 'application/json'
+  content_type 'application/json'
 end
 
 get '/' do
   'Master Ruby Web APIs - Chapter 2'
 end
 
-get '/users' do
-  users.map { |name, data| data.merge(id: name) }.to_json
+# Looping through the list of routes for which
+# we want to return a JSON representation.
+['/users', '/users.json'].each do |path|
+  get path do
+    users.map { |name, data| data.merge(id: name) }.to_json
+  end
+end
+
+# Defining /users.xml with the application/xml media type
+# and simply calling Gyoku on our users hash with a root
+# element 'users'.
+get '/users.xml' do
+  content_type 'application/xml'
+  Gyoku.xml(users: users)
 end
